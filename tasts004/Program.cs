e4eb76fb5873212664ecd579d63cfc1462a54a2c
Anastasia <didyoumissme.19s@gmail.com>
2023-06-15 00:57:52 +0300

    Добавтлт решение tests004

---
+++ tasts004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N) , а на выходе показывает все чёные числа от 1, до N
+Console.Write("Введите первое число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if(N>=2){
+while (i<=N)
+{
+    if (i%2==0){Console.Write(i);}
+    i=i+2;
+    
+};}
+else if(N<2){Console.Write(" ");}
+Console.ReadKey();
