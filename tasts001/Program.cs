14496627271dd24918286f863615cafc34893b02
Anastasia <didyoumissme.19s@gmail.com>
2023-06-15 00:26:05 +0300

    Добавили решение tests002

--- tasts001/Program.cs
+++ tasts001/Program.cs
@@ -1,9 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-//a = 
+﻿// Напишите программу, которая на вход принимает два числа и выдает какое из них большее, а какое меньшее 
 Console.Write("Введите первое число:");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberA>numberB) {Console.Write(numberA + ">" +numberB);}
-else{Console.Write(numberA + "<" +numberB);}
+if (numberA>numberB) {Console.Write(numberA + ">" +numberB + ", max =" + numberA);}
+else{Console.Write(numberA + "<" +numberB + ", max =" + numberB);}
 Console.ReadKey();
---
+++ tasts002/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+Console.Write("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB && numberA>numberC) {Console.Write("max=" + numberA);}
+else if (numberB>numberA && numberB>numberC) {Console.Write("max=" + numberB);}
+else if (numberC>numberA && numberC>numberB) {Console.Write("max=" + numberC);}
+Console.ReadKey();
