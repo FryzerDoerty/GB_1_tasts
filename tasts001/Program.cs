7cc67871127550e63f2a034a6a0a8104922c6770
Anastasia <didyoumissme.19s@gmail.com>
2023-06-15 00:09:01 +0300

    добавили решение задачи 1

---
+++ tasts001/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//a = 
+Console.Write("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB) {Console.Write(numberA + ">" +numberB);}
+else{Console.Write(numberA + "<" +numberB);}
+Console.ReadKey();
